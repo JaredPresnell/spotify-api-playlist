c9e68350f2ec0cc3ca50fa45de628089f7eb3224
JaredPresnell <jared.presnell@gmail.com>
2019-04-17 16:31:18 -0500

    public file?

--- src/Components/TopSongs.js
+++ src/Components/TopSongs.js
@@ -21,26 +21,31 @@ export default class TopSongs extends Component {
     return artistsTotal;
   }
   render(){
+    
     return (
       <div className="playlist_container">
         <h1 className="playlist_title">Current Playlist</h1>
-        <p>This is the current playlist, with the names of the song artists and the Spotify user who contributed each song.</p>
+        <p className="description">Below is the current up to date playlist, with the names of each song's artists and the Spotify user who contributed each song.</p>
         <h4>Last updated: {this.mapLastUpdated(this.props.lastUpdated)}</h4>
-        {/*<li key={i}>{item.name}, {item.artists[0].name}, {item.user}</li> */}
         <ul>
           {
-            //console.log(this.props.tracks)
-            this.props.tracks.map((item, i) => 
-              
-              <div key={item.name + i} className="container">
-                <div className="flex-item song_item">
-                  {item.name} by {this.mapArtists(item.artists)}
+            this.props.tracks.map((item, i) => {
+              var addDividerClass="container ";
+              var lastName = i>1 ?  this.props.tracks[i-1].name : item.name; 
+              if(lastName !== item.name) addDividerClass += "dividerClass";
+              //if(i==5) addDividerClass += "dividerClass"; //for testing 
+              return(
+                  <div key={item.name + i} className={addDividerClass}>
+                  {console.log(item)}
+                  <div className="flex-item song_item">
+                    {item.name} by {this.mapArtists(item.artists)}
+                  </div>
+                  <div className="flex-item">
+                    {item.user}
+                  </div>
                 </div>
-                <div className="flex-item">
-                  {item.user}
-                </div>
-              </div>  
-              )
+              )}
+            )
           }   
         </ul>
         {/*<button onClick = {()=> this.props.getTracks()}>Get Playlist</button>*/}
